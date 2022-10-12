9aa3ffa779d8bc277d622e6be4d293f10d8c00e6
ffsong <ffsong@che300.com>
2022-10-12 11:53:12 +0800

    feat: 修改一些配置

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -70,16 +70,16 @@ export default hopeTheme({
   //https://vuepress-theme-hope.github.io/v2/zh/config/theme/feature.html
   blog: {
     articleInfo: ["Date", "Category", "Tag", "ReadingTime", "PageView"],
-    name: "清顺",
+    name: "FEIFFY",
     avatar: "/avatar_self.webp",
     description: "迷信新工具，热衷于研究开源软件、心理学理论，定期分享探索成果",
     intro: "/intro.html",
     roundAvatar: true,
     medias: {
-      GitHub: "https://github.com/rockbenben",
-      Zhihu: "https://www.zhihu.com/people/qingwhat",
-      Weibo: "https://weibo.com/u/1992817087",
-      Wechat: "http://tc.seoipo.com/wechat.svg",
+      GitHub: "https://github.com/feiffy",
+      Zhihu: "",
+      Weibo: "",
+      Wechat: "",
       少数派: [
         "https://sspai.com/u/zqj05i4v/posts",
         path.resolve(__dirname, "icons/sspai.svg"),
